6644d907604fba33d38e8e1e9d3afd41cb2fbd64
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2023-08-29 14:31:22 +0700

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>X</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100 bg-light">
+  <nav class="navbar shadow sticky-top nav-underline navbar-expand-lg navbar-light ps-3" style="background-color: #A3C7E9">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="assets/img/logo.png" width="100" height="30" alt="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav me-auto mb-2 ms-4 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" aria-current="page" href="#">Beranda</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle fw-bold" style="color: #134F5C" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Semester
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Semester 1</a></li>
+              <li><a class="dropdown-item" href="#">Semester 2</a></li>
+              <li><a class="dropdown-item" href="#">Semester 3</a></li>
+              <li><a class="dropdown-item" href="#">Semester 4</a></li>
+              <li><a class="dropdown-item" href="#">Semester 5</a></li>
+              <li><a class="dropdown-item" href="#">Semester 6</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="#">Upload Soal</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
