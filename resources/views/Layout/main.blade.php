b32e74a5408ee10b4a694dd6f92e0ada0ae2a48c
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2023-08-31 14:58:23 +0700

    tambahan

---
+++ resources/views/Layout/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>X</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100 bg-light">
+  <nav class="navbar shadow sticky-top nav-underline navbar-expand-lg navbar-light ps-3" style="background-color: #A3C7E9">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="assets/img/logo.png" width="100" height="30" alt="logo">
+      </a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 ms-4 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C"  href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="#">Guru dan Staff</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="#">Jurusan</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <footer class="bg-white text-center text-white mt-auto">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0 ">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-facebook"></ion-icon>
+                </a>
+
+                <!-- Twitter -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-twitter"></ion-icon>
+                </a>
+
+                <!-- Google -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-google"></ion-icon>
+                </a>
+
+                <!-- Instagram -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-instagram"></ion-icon>
+                </a>
+
+                <!-- Linkedin -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-linkedin"></ion-icon>
+                </a>
+
+                <!-- Github -->
+                <a href="https://github.com/Amar-Suhendra/INF206-2022-DY2-MengHeal/graphs/contributors" target="_blank" class="btn">
+                    <ion-icon size="large" name="logo-github"></ion-icon>
+                </a>
+                
+            </section>
+            <!-- Section: Social media -->
+        </div>
+
+</body>
+
+</html>
