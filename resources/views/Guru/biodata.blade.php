72a332cde3bf5bdefefc6d564abbea97687eab07
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2023-08-31 16:33:44 +0700

    tambah

---
+++ resources/views/Guru/biodata.blade.php
@@ -0,0 +1,13 @@
+@extends('Guru.navbarguru')
+@section('container')
+    <div class="container">
+        <h1>Biodata</h1>
+        <p><strong>Nama:</strong> John Doe</p>
+        <p><strong>Usia:</strong> 30 tahun</p>
+        <p><strong>Alamat:</strong> Jl. Contoh No. 123, Kota Contoh</p>
+        <p><strong>Email:</strong> johndoe@example.com</p>
+        <p><strong>Telepon:</strong> 123-456-7890</p>
+        <p><strong>Pendidikan:</strong> Sarjana Teknik Informatika</p>
+        <p><strong>Hobi:</strong> Membaca, Berenang</p>
+    </div>
+@endsection
---
+++ resources/views/Guru/navbarguru.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>X</title>
+</head>
+
+<body class="d-flex flex-column bg-light">
+    <nav class="navbar shadow sticky-top navbar-expand-lg navbar-light ps-3" style="background-color: #A3C7E9">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="#" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                SMK
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5 mb-5">
+        @yield('container')
+    </div>
+</body>
+</html>
