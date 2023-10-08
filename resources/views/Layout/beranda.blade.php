30e30768468f4629bbf970948d09cda51e69f214
Zhilal <teuku_zm20@mhs.unsyiah.ac.id>
2023-10-08 22:00:44 +0700

    Beranda polish1

--- resources/views/Layout/beranda.blade.php
+++ resources/views/Layout/beranda.blade.php
@@ -1,28 +1,32 @@
 @extends('Layout.main')
 @section('container')
     <div class="intro">
-        <section class="banner">
             <h2>Selamat Datang di Sekolah Menegah Kejurusan  Banda Aceh</h2>
-        </section>
-        <section class="tentang">
-            <p>Kami adalah sekolah yang berfokus pada pengembangan potensi siswa dalam berbagai bidang kesenian dan manajemen bisnis</p>
-        </section>
+            <p> Kami adalah sekolah yang berfokus pada pengembangan potensi siswa dalam berbagai bidang kesenian dan manajemen bisnis<br>
+                Dengan banyak fasilitas yang dapat digunakan oleh siswa seperti;.......<br>
+                Guru yang terbaik di indonesia, mari belajar bersama dengan para siswa yang telah bergabung!
+            </p>
     </div>
 
     <div class="prestasi">
         <h2>Prestasi SMK  Banda Aceh</h2>
+        <p>Mari lihat prestasi yang telah didapatkan oleh siswa dan alumni yang telah bergabung!</p>
         <div class="container text-center">
             <div class="row align-items-start">
                 <div class="box col">
                     <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
+                    <a href="https://aceh.tribunnews.com/2023/01/16/smk-negeri-3-banda-aceh-ikut-meriahkan-kedatangan-penumpang-kapal-pesiar-mewah-di-sabang">SMK mendapatkan prestasi berikut dari melakukan</a>
                 </div>
                 <div class="box col">
                     <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
+                    <a href="https://aceh.tribunnews.com/2023/01/16/smk-negeri-3-banda-aceh-ikut-meriahkan-kedatangan-penumpang-kapal-pesiar-mewah-di-sabang">SMK mendapatkan prestasi berikut dari melakukan</a>
                 </div>
                 <div class="box col">
                     <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
+                    <a href="https://aceh.tribunnews.com/2023/01/16/smk-negeri-3-banda-aceh-ikut-meriahkan-kedatangan-penumpang-kapal-pesiar-mewah-di-sabang">SMK mendapatkan prestasi berikut dari melakukan</a>
                 </div>
             </div>
         </div>
+        <h3>AYO! Raihlah prestasi lebih bagus lagi untuk SMK</h3>
     </div>
 @endsection
