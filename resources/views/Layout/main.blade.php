90a0f54f9792674b8b2241bd32e3c2d5a1789306
Zhilal <teuku_zm20@mhs.unsyiah.ac.id>
2023-10-08 14:33:17 +0700

    Beranda lalu

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -3,18 +3,43 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <link href="css/beranda.css" rel="stylesheet">
-  <title>Management SMK</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Management SMK</title>
 </head>
 
-<body>
-  <ul>
-  <li><a href="/login">Login</a></li>
-  <li><a href="">Jurusan</a></li>
-  <li><a href="">Guru dan staff</a></li>
-  <li><a class="active" href="/">Beranda</a></li> 
-  </ul>
+<body class="d-flex flex-column min-vh-100 bg-light">
+  <nav class="navbar shadow sticky-top nav-underline navbar-expand-lg navbar-light ps-3" style="background-color: #A3C7E9">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="assets/img/logo.png" width="100" height="30" alt="logo">
+      </a>
+      <div class="collapse navbar-collapse nav justify-content-end">
+        <ul class="navbar-nav mb-2 ms-4 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C"  href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="#">Guru dan Staff</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="#">Jurusan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" style="color: #134F5C" href="/login">Login</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <footer class="bg-white text-center text-white mt-auto">
+    
+</footer>
+
 </body>
 
 </html>
