2b833d898606228c5a319cf3b9508a3628248bd4
Zhilal <teuku_zm20@mhs.unsyiah.ac.id>
2023-10-08 21:42:42 +0700

    Jurusan

--- resources/views/Layout/beranda.blade.php
+++ resources/views/Layout/beranda.blade.php
@@ -2,7 +2,7 @@
 @section('container')
     <div class="intro">
         <section class="banner">
-            <h2>Selamat Datang di Sekolah Menegah Kejurusan 3 Banda Aceh</h2>
+            <h2>Selamat Datang di Sekolah Menegah Kejurusan  Banda Aceh</h2>
         </section>
         <section class="tentang">
             <p>Kami adalah sekolah yang berfokus pada pengembangan potensi siswa dalam berbagai bidang kesenian dan manajemen bisnis</p>
@@ -10,17 +10,17 @@
     </div>
 
     <div class="prestasi">
-        <h2>Prestasi SMK 3 Banda Aceh</h2>
+        <h2>Prestasi SMK  Banda Aceh</h2>
         <div class="container text-center">
             <div class="row align-items-start">
                 <div class="box col">
-                    <p>Berikut ini prestasi SMK3 dalam meraih penghargaan</p>
+                    <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
                 </div>
                 <div class="box col">
-                    <p>Berikut ini prestasi SMK3 dalam meraih penghargaan</p>
+                    <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
                 </div>
                 <div class="box col">
-                    <p>Berikut ini prestasi SMK3 dalam meraih penghargaan</p>
+                    <p>Berikut ini prestasi SMK dalam meraih penghargaan</p>
                 </div>
             </div>
         </div>
--- resources/views/Layout/guru.blade.php
+++ resources/views/Layout/guru.blade.php
@@ -1,37 +1,34 @@
 @extends('Layout.main')
 @section('container')
     <div class="guru">
-        <h3>Guru SMK3 Banda Aceh</h3>
+        <h3>Guru SMK Banda Aceh</h3>
         <div class="container text-center">
             <div class="row align-items-start">
                 <div class="box col">
-                    <p>Saya adalah guru dalam SMK3 dalam mengajarkan pelajaran</p>
+                    <p>Saya adalah guru dalam SMK dalam mengajarkan pelajaran</p>
                 </div>
                 <div class="box col">
-                    <p>Saya adalah guru dalam SMK3 dalam mengajarakan pelajaran</p>
+                    <p>Saya adalah guru dalam SMK dalam mengajarakan pelajaran</p>
                 </div>
                 <div class="box col">
-                    <p>Saya adalah guru dalam SMK3 dalam mengajarkan pelajaran</p>
-                </div>
-                <div class="box col">
-                    <p>Saya adalah guru dalam SMK3 dalam mengajarkan pelajaran</p>
+                    <p>Saya adalah guru dalam SMK dalam mengajarkan pelajaran</p>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="staff">
-        <h3>Staff SMK3 Banda Aceh</h3>
+        <h3>Staff SMK Banda Aceh</h3>
         <div class="container text-center">
             <div class="row align-items-start">
                 <div class="box col">
-                    <p>Saya adalah staff dalam SMK3 dalam mengajarkan pelajaran</p>
+                    <p>Saya adalah staff dalam SMK dalam mengajarkan pelajaran</p>
                 </div>
                 <div class="box col">
-                    <p>Saya adalah staff dalam SMK3 dalam mengajarakan pelajaran</p>
+                    <p>Saya adalah staff dalam SMK dalam mengajarakan pelajaran</p>
                 </div>
                 <div class="box col">
-                    <p>Saya adalah staff dalam SMK3 dalam mengajarkan pelajaran</p>
+                    <p>Saya adalah staff dalam SMK dalam mengajarkan pelajaran</p>
                 </div>
             </div>
         </div>
